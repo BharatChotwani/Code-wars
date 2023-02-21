b7c61d49e9965d86396bb89c52e06753f0a3a496
Bharat <bchotwani3@gmail.com>
2023-02-21 20:42:58 +0530

    initial

--- index.js
+++ index.js
@@ -19,3 +19,24 @@ function century(year) {
 }
 const value = century(19);
 console.log(value);
+// Operators, Branching, Loops
+//Q.3 Write a program to add 5 numbers. The value of numbers are num1=5, num2=13, num3=7, num4=21 and num5=48.
+const num1 = 5;
+const num2 = 13;
+const num3 = 7;
+const num4 = 21;
+const num5 = 48;
+const add = num1 + num2 + num3 + num4 + num5;
+console.log(add);
+//Q.4 Write a program to take a number input from user and determine whether the number is odd or even.
+function oddAndEven(n) {
+  if (n % 2 === 0) {
+    console.log(`Even Number`);
+  } else console.log(`Odd Number`);
+}
+const oddEven = (n) => {
+  return n % 2 === 0 ? console.log(`Even Number`) : console.log(`Odd Number`);
+};
+
+oddAndEven(18);
+oddEven(27);
